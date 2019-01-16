b7a6c8932705761ac014fb024b54cdc0068da4f5
SickanEkman <sara.d.ekman@gmail.com>
2019-01-16 13:07:07 +0100

    Add docstrings

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 def open_file(filename):
+    """
+    Open file and return content
+    :param filename: Full path to file as a string
+    :return: A string with file content, all lower case
+    """
     with open(filename, "r") as fin:
         data = fin.read()
         data = data.lower()
@@ -6,11 +11,23 @@ def open_file(filename):
 
 
 def string_to_list(text):
+    """
+    Convert a string to a list, splitting on line break
+    :param text: A string
+    :return: A list
+    """
     the_list = text.split("\n")
     return the_list
 
 
 def clean_text(a_list):
+    """
+    Remove trailing spaces after list items,
+    remove list elements only containing a space,
+    remove empty list elements
+    :param a_list: A list
+    :return: A cleaned list
+    """
     no_trailing_spaces = map(lambda i: i.strip(), a_list)
     no_empty_items = filter(lambda i: i != i.isspace(), no_trailing_spaces)
     cleaned_text = filter(lambda i: i != "", no_empty_items)
