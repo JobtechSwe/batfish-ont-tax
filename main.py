3d00add6f4769b1af65d823bfdb208c0fc8ab4db
SickanEkman <sara.d.ekman@gmail.com>
2019-01-22 17:10:37 +0100

    Add basic compound splitter

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from spyro_compound_splitting import compound_splitter
 import exact_matches
-import compound_splitter
 
 tax_file = "resources/occupations_from_legacy_taxonomy.txt"  # path to file with words from Taxonomy
 dm_file = "resources/ontology_all_occupations.txt"  # path to file with words from data mining
 testfile = "resources/testdata.txt"
 
 if __name__ == "__main__":
-    #matched_words, not_matched_dm_words, not_matched_t_words = exact_matches.match(tax_file, dm_file)
-    compound_splitter.split(testfile)
+    matched_words, not_matched_dm_words, not_matched_t_words = exact_matches.match(tax_file, dm_file)
+    for word in not_matched_t_words:
+        compound_splitter.do_split(word, "NN")
---
+++ spyro_compound_splitting/compound_splitter.py
@@ -0,0 +1,4 @@
+from .spyro.splitter import do_split
+
+def test_splitter(word, pos):
+    do_split(word, pos)
